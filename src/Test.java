599b24f1ff7da03fd8d6fc332bce6e1a92ddcbb9
Ivan Ivanov <79523701529@yandex.ru>
2023-01-06 12:49:00 +0300

    HashSet, LinkedHashSet, TreeSet

--- src/Test.java
+++ src/Test.java
@@ -2,23 +2,25 @@ import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-       Map<Integer,String>hashMap = new HashMap<>();
-       Map<Integer,String>linkedHashMap = new LinkedHashMap<>();
-       Map<Integer,String>treeMap = new TreeMap<>();
+       Set<String>hashSet = new HashSet<>();
+       Set<String>linkedHashSet = new LinkedHashSet<>();
+       Set<String>treeSet = new TreeSet<>();
 
-        testMap(hashMap);      //does not preserve the order in which the element was added
-        System.out.println();
-        testMap(linkedHashMap);//preserve the order
-        System.out.println();
-        testMap(treeMap);      //sort by key
+       testSet(hashSet);
+       System.out.println();
+       testSet(linkedHashSet);
+       System.out.println();
+       testSet(treeSet);
     }
-     public static void testMap(Map<Integer,String>map){
-         map.put(22,"Bob");
-         map.put(11,"Bob");
-         map.put(33,"Mike");
+    public static void testSet(Set<String>set){
+        set.add("Zak");
+        set.add("Mike");
+        set.add("Don");
+        set.add("Tom");
+        set.add("Aron");
 
-         for (Map.Entry<Integer,String>entry:map.entrySet()){
-             System.out.println(entry.getKey()+" : "+entry.getValue());
-         }
-     }
+        for (String a:set){
+            System.out.print(a);
+        }
+    }
 }
